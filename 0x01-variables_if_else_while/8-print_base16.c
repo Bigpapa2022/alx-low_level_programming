fd3450b13c52f81351ef8d1e8898b0864dc8cf1d
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-01 14:29:52 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: End of program
+ *(Program by Hayzed)
+ */
+int main(void)
+{
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{putchar(base_16);
+	}
+
+	for (base_16 = 97 ; base_16 <= 102; base_16++)
+	{
+		putchar(base_16);
+	}
+llputchar ('\n');
+
+	return (0);
+}
