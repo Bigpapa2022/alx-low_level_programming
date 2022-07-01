84f8f10be53c26fae778e4fe9f66cb597e7fa5b4
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-01 14:12:04 -0700

    Tas 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: End of program
+ * (Program by Hayzed)
+ */
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 122; ch >= 97; ch--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
