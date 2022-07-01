896fd49a23110f152b02537e36b657dee7a335e3
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-01 13:10:08 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: End of program
+ *(Program by Hayzed)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0
+		;
+	while (i < 10)
+	{
+		printf("%i\n", i);
+		i++
+	}
+	return (0);
+}
