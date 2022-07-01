43fea0ad3812d7849ca811ea70ef26638532c57c
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-01 11:57:33 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: End of program
+ *(Program by Hayzed)
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper  <= 'Z'; upper++)
+	{
+		putchar(upper_case);
+	}
+	putchar ('\n');
+	return (0);
+}
