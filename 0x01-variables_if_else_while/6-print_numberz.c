b33ca3fe3f29384c2c2a37a52d4d02f010311cae
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-01 13:51:20 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: End of program
+ *(Program by Hayzed)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 18; i <= 27; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
+}
