d75290b5cac37b35fa1806b2ad4b5145e144e6cb
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-05 12:43:17 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_sign - Program prints the sign of a number
+*return 0 letter not lowercase, 1 letter lowercase
+ *@n: the int to print
+ *(Code by Hayzed)
+ *Return: End of Program.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+else
+{
+_putchar ('-');
+return (-1);
+}
+}
