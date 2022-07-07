7a34b3abe74f2b2a554e910f3c07748822552a55
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-07 13:02:44 -0700

    Task on more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -multiplies two integers
+ *@a: print int
+ *@b: print int
+ * Return: Always 0.
+ * (Code by Hayzed)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
