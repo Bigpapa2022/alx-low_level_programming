3e06e7ddd5d9986cdceb8849601f533b205398a1
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-07 12:21:08 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* print_number - that prints an integer
+*@n: print int
+* Return: Always 0.
+* (Code by Hayzed)
+*/
+void print_number(int n)
+{
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# MORE TASKS AND EXPLANATION ON FUNCTIONS AND NESTED LOOPS
+This directory gives more explanation and more examples on nested loops
