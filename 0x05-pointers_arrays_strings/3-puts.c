50df741820e6b97e6919dc61c9d139bd300e5de6
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-12 14:31:56 -0700

    Task 2 & 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio>
+#include "main.h"
+/**
+*_puts - prints a string
+*@str: A pointer to a char that will be changed
+*(Code by Hayzed)
+*Return: void which means our answer is correct
+*/
+
+void _puts(char *str)
+{
+char *c;
+int m;
+
+c = str;
+
+for (m = 0; c[m]; m++)
+{
+printf("%s\n", c[m]);
+}
