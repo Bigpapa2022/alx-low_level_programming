55dfcb2af7a9cb0ad963741d96246ac6938de31f
Abdulazeez <ogunnubiademola1@gmail.com>
2022-07-12 12:26:59 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a  value integer to 98
+ *@n: A pointer to an int that will be updated
+ *Return: void that means is correct
+ *(Code by Hayzed)
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# C - POINTERS , ARRAYS AND STRINGS
+This repository show examples on basic pionters in c, how pointers work and how it is used. It also show's examples on arrays and strings.
